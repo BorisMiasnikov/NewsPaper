78362dcab67e8d751b973e623ba96c90e8fd4e64
Boris Miasnikov <Jason2297@mail.ru>
2024-04-28 15:38:36 +0300

    Добавлены задачи через селери. Старые отправки через сигналы закоментированы

--- news/urls.py
+++ news/urls.py
@@ -1,20 +1,22 @@
 from django.urls import path
 
+from django.views.decorators.cache import cache_page
+
 # Импортируем созданное нами представление
 from .views import PostsList, PostDetail, PostsSearch, NewsCreate, PostUpdate, PostDelete, ArticlesCreate, \
     CategoriesPostList, subscribe, IndexView
 
 urlpatterns = [
-    path('', PostsList.as_view(), name= 'Posts_list'),
-    path('<int:pk>/', PostDetail.as_view(), name= 'Post_detail'),
+    path('', cache_page(60)(PostsList.as_view()), name= 'Posts_list'),
+    path('<int:pk>/', cache_page(300)(PostDetail.as_view()), name= 'Post_detail'),
     path('search/', PostsSearch.as_view(), name= 'Posts_search'),
-    path('news/create/', NewsCreate.as_view(), name='News_create'),
+    path('news/create/', cache_page(1000)(NewsCreate.as_view()), name='News_create'),
     path('news/<int:pk>/edit/', PostUpdate.as_view(), name='News_edit'),
     path('news/<int:pk>/delete/', PostDelete.as_view(), name='News_delete'),
-    path('articles/create/', ArticlesCreate.as_view(), name='Articles_create'),
+    path('articles/create/', cache_page(1000)(ArticlesCreate.as_view()), name='Articles_create'),
     path('articles/<int:pk>/edit/', PostUpdate.as_view(), name='Articles_edit'),
     path('articles/<int:pk>/delete/', PostDelete.as_view(), name='Articles_delete'),
-    path('categories/<int:pk>/', CategoriesPostList.as_view(), name='Category_list'),
+    path('categories/<int:pk>/', cache_page(60)(CategoriesPostList.as_view()), name='Category_list'),
     path('categories/<int:pk>/subscribe', subscribe, name='Subscribe'),
     path('hello',IndexView.as_view()), #эта страница для проверки работы IndexView
 ]

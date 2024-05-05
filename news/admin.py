ae9b7d0ebc1897363ea4a5bb55bb220a1d59b030
Boris Miasnikov <Jason2297@mail.ru>
2024-05-05 20:27:23 +0300

    Добавлены задачи через селери. Старые отправки через сигналы закоментированы

--- news/admin.py
+++ news/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from .models import Post, Author, Comment, Category
 
-admin.site.register(Post)
+def nullfy_rating(modeladmin, request, queryset):#функция обнуления рейтинга
+    #request — объект хранящий информацию о запросе и queryset — грубо говоря набор объектов, которых мы выделили галочками.
+    queryset.update(rating=0)
+    nullfy_rating.short_description = 'Обнулить рейтинг'  # описание для более понятного представления в админ панеле задаётся, как будто это объект
+
+class PostAdmin(admin.ModelAdmin):# создаём новый класс для представления товаров в админке
+    # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
+    list_display = ['title', 'data_in', 'rating', 'author', 'category' ]
+    list_filter = ('author', 'data_in', 'category')#добавляем примитивные фильтры
+    search_fields = ('title', 'category__category' )
+    actions = [nullfy_rating]
+
+    def category(self, obj):
+        return ", ".join(cat.category for cat in obj.PostCategory.all())
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Category)
 admin.site.register(Comment)
+
